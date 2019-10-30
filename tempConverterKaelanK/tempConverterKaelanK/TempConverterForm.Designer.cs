30556101b76fa15cbc6537ab7ef8370b8eaaea14
Kaelan Khan <32547155+KaelanKhan@users.noreply.github.com>
2019-10-30 13:02:05 -0400

    Initial commit

---
+++ tempConverterKaelanK/tempConverterKaelanK/TempConverterForm.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace tempConverterKaelanK
+{
+    partial class frmTempConverter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblInstruc = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblInstruc
+            // 
+            this.lblInstruc.AutoSize = true;
+            this.lblInstruc.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInstruc.Location = new System.Drawing.Point(25, 24);
+            this.lblInstruc.Name = "lblInstruc";
+            this.lblInstruc.Size = new System.Drawing.Size(221, 20);
+            this.lblInstruc.TabIndex = 0;
+            this.lblInstruc.Text = "Input a temperature in Celsius\r\n";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.Location = new System.Drawing.Point(29, 67);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(85, 33);
+            this.btnCalculate.TabIndex = 2;
+            this.btnCalculate.Text = "Convert";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.BtnCalculate_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(252, 24);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // frmTempConverter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(403, 146);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.lblInstruc);
+            this.Name = "frmTempConverter";
+            this.Text = "Teperature Converter by Kaelan K";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblInstruc;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ tempConverterKaelanK/tempConverterKaelanK/TempConverterForm.cs
@@ -0,0 +1,51 @@
+﻿/*
+ * Created by: Kaelan Khan
+ * Created on: 30-10-2019
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #30 - Area Procedure
+ * This program takes the users celsius and converts it to fahrenheit
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tempConverterKaelanK
+{
+    public partial class frmTempConverter : Form
+    {
+        public frmTempConverter()
+        {
+            InitializeComponent();
+        }
+
+        public void CalculateF(double celsius)
+        {
+            // declare local var
+            double convertedTemp;
+
+            // calculate the converted temp
+            convertedTemp = 9 / 5 * celsius + 32;
+
+            // display a message box
+            MessageBox.Show(celsius + " Celsius" + " = " + convertedTemp + " Fahrenheit");
+        }
+        private void BtnCalculate_Click(object sender, EventArgs e)
+        {
+            // declare variable
+            double celsius;
+
+            // convert user input to number
+            celsius = Double.Parse(this.textBox1.Text);
+
+            // call on calculateF
+            this.CalculateF(celsius);
+
+        }
+    }
+}
