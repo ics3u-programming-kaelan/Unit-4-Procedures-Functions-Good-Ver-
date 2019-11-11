85dfb7e46b3437294643115e4f45a012ab3627da
Kaelan Khan <32547155+KaelanKhan@users.noreply.github.com>
2019-11-11 13:09:34 -0500

    finished switch statement

---
+++ switchStatementKaelanK/switchStatementKaelanK/percentageForm.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace switchStatementKaelanK
+{
+    partial class frmPercentage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblInput = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnEnter = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblInput
+            // 
+            this.lblInput.AutoSize = true;
+            this.lblInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInput.Location = new System.Drawing.Point(28, 45);
+            this.lblInput.Name = "lblInput";
+            this.lblInput.Size = new System.Drawing.Size(110, 20);
+            this.lblInput.TabIndex = 0;
+            this.lblInput.Text = "Input A Value:";
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(144, 47);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(100, 20);
+            this.txtInput.TabIndex = 1;
+            // 
+            // btnEnter
+            // 
+            this.btnEnter.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEnter.Location = new System.Drawing.Point(89, 90);
+            this.btnEnter.Name = "btnEnter";
+            this.btnEnter.Size = new System.Drawing.Size(102, 38);
+            this.btnEnter.TabIndex = 2;
+            this.btnEnter.Text = "Enter";
+            this.btnEnter.UseVisualStyleBackColor = true;
+            this.btnEnter.Click += new System.EventHandler(this.BtnEnter_Click);
+            // 
+            // frmPercentage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(302, 162);
+            this.Controls.Add(this.btnEnter);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.lblInput);
+            this.Name = "frmPercentage";
+            this.Text = "Switch by Kaelan K";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblInput;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnEnter;
+    }
+}
+
---
+++ switchStatementKaelanK/switchStatementKaelanK/percentageForm.cs
@@ -0,0 +1,119 @@
+﻿/*
+ * Created by: Kaelan K
+ * Created on: 11-11-2019
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #32 - Percentage Program
+ * This program takes a users level and converts it to a percentage
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace switchStatementKaelanK
+{
+    public partial class frmPercentage : Form
+    {
+        public frmPercentage()
+        {
+            InitializeComponent();
+        }
+
+        private int ConvertGrade (String grade)
+        {
+            // setting func var
+            int percentage;
+
+            // check to convert grade to percentage
+            switch (grade)
+            {
+                case "4++":
+                    percentage = 98;
+                    break;
+                case "4+":
+                    percentage = 95;
+                    break;
+                case "4":
+                    percentage = 85;
+                    break;
+                case "4-":
+                    percentage = 80;
+                    break;
+                case "3+":
+                    percentage = 78;
+                    break;
+                case "3":
+                    percentage = 75;
+                    break;
+                case "3-":
+                    percentage = 72;
+                    break;
+                case "2+":
+                    percentage = 68;
+                    break;
+                case "2":
+                    percentage = 65;
+                    break;
+                case "2-":
+                    percentage = 62;
+                    break;
+                case "1+":
+                    percentage = 58;
+                    break;
+                case "1":
+                    percentage = 55;
+                    break;
+                case "1-":
+                    percentage = 52;
+                    break;
+                case "0+":
+                    percentage = 45;
+                    break;
+                case "0":
+                    percentage = 40;
+                    break;
+                case "0-":
+                    percentage = 35;
+                    break;
+                case "0--":
+                    percentage = 30;
+                    break;
+                case "0---":
+                    percentage = 20;
+                    break;
+                case "0----":
+                    percentage = 10;
+                    break;
+                default:
+                    percentage = -1;
+                    break;
+
+            }
+
+            // return value
+            return percentage;
+
+        }
+
+        private void BtnEnter_Click(object sender, EventArgs e)
+        {
+            // declaring vars
+            string userGrade;
+            int userPercentage;
+
+            // set user grade to text value
+            userGrade = this.txtInput.Text;
+
+            // call function
+            userPercentage = ConvertGrade(userGrade);
+
+            // display message box with returned value
+            MessageBox.Show("Your mark as a percentage is a " + userPercentage);
+        }
+    }
+}
