425cade1ef21399e1a1dd24abc948bff4866ccc7
Kaelan Khan <32547155+KaelanKhan@users.noreply.github.com>
2019-11-13 13:10:14 -0500

    pass by ref

---
+++ passByRefKaelanK/passByRefKaelanK/passByRefForm.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace passByRefKaelanK
+{
+    partial class passByRefForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picCard = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picCard)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picCard
+            // 
+            this.picCard.Image = global::passByRefKaelanK.Properties.Resources.QS;
+            this.picCard.Location = new System.Drawing.Point(-3, 0);
+            this.picCard.Name = "picCard";
+            this.picCard.Size = new System.Drawing.Size(352, 470);
+            this.picCard.TabIndex = 0;
+            this.picCard.TabStop = false;
+            this.picCard.Click += new System.EventHandler(this.picCard_Clicked);
+            // 
+            // passByRefForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(347, 471);
+            this.Controls.Add(this.picCard);
+            this.Name = "passByRefForm";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picCard)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picCard;
+    }
+}
+
---
+++ passByRefKaelanK/passByRefKaelanK/passByRefForm.cs
@@ -0,0 +1,100 @@
+﻿/*
+ * Created by: Kaelan K
+ * Created on: 13-11-2019
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #33 - Name of Program
+ * This program...
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace passByRefKaelanK
+{
+    public partial class passByRefForm : Form
+    {
+        Random randNumberGenerator = new Random();
+
+        public passByRefForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ChangeCard (ref PictureBox aPictureBox, int cardNumber)
+        {
+            if (cardNumber == 1)
+            {
+                aPictureBox.Image = Properties.Resources.Front;
+            }
+            else if (cardNumber == 2)
+            {
+                aPictureBox.Image = Properties.Resources.JC;
+            }
+            else if (cardNumber == 3)
+            {
+                aPictureBox.Image = Properties.Resources.JD;
+            }
+            else if (cardNumber == 4)
+            {
+                aPictureBox.Image = Properties.Resources.JH;
+            }
+            else if (cardNumber == 5)
+            {
+                aPictureBox.Image = Properties.Resources.JS;
+            }
+            else if (cardNumber == 6)
+            {
+                aPictureBox.Image = Properties.Resources.KC;
+            }
+            else if (cardNumber == 7)
+            {
+                aPictureBox.Image = Properties.Resources.KD;
+            }
+            else if (cardNumber == 8)
+            {
+                aPictureBox.Image = Properties.Resources.KH;
+            }
+            else if (cardNumber == 9)
+            {
+                aPictureBox.Image = Properties.Resources.KS;
+            }
+            else if (cardNumber == 10)
+            {
+                aPictureBox.Image = Properties.Resources.QC;
+            }
+            else if (cardNumber == 11)
+            {
+                aPictureBox.Image = Properties.Resources.QD;
+            }
+            else if (cardNumber == 12)
+            {
+                aPictureBox.Image = Properties.Resources.QH;
+            }
+            else if (cardNumber == 13)
+            {
+                aPictureBox.Image = Properties.Resources.QS;
+            }
+        }
+
+        private void picCard_Clicked(object sender, EventArgs e)
+        {
+            // declaring local constant
+            const int MAX_NUM = 13;
+
+            // declaring local var
+            int aRandomNumber;
+
+            // get random number between 1 and 13
+            aRandomNumber = randNumberGenerator.Next(1, MAX_NUM + 1);
+
+            // call function
+            ChangeCard(ref this.picCard, aRandomNumber);
+        }
+    }
+}
